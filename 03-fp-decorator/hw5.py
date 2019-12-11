55e65760f49a0544a8d13d89b82a03897ba78b2b
ilineserg <45879914+ilineserg@users.noreply.github.com>
2019-12-12 00:45:37 +0300

    Update hw5.py

--- 03-fp-decorator/hw5.py
+++ 03-fp-decorator/hw5.py
@@ -54,7 +54,8 @@ def fib_4(n):
 
 def pow(x, n, I, mult):
     """
-    Returns x ** n. Suggests that I is unit matrix, which is multiplied with mult and n is positive number.
+    Returns x ** n. Suggests that I is unit matrix, 
+    which is multiplied with mult and n is positive number.
     """
     if n == 0:
         return I
