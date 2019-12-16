699a08ec445c3f74dfefa275e42a7a102c1a029b
Sergey ILIN <ilineserg@gmail.com>
2019-12-16 21:59:21 +0300

    oop is done

--- 09-HTTP/pikabu_parser.py
+++ 09-HTTP/pikabu_parser.py
@@ -2,23 +2,43 @@ from bs4 import BeautifulSoup
 import requests
 from requests.auth import HTTPBasicAuth
 
+articles_of_stories = []
 
-user_agent = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
-      }
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Connection": "keep-alive",
+    "cache-control": "max-age=0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+    "cookie": "pkbRem=%7B%22uid%22%3A3011766%2C%22username%22%3A%22ilineserg%22%2C%22rem%22%3A%226aebdf22bb6e139da1a0dc5b033ae8de%22%2C%22tries%22%3A0%7D"
+}
 
 username = 'ilineserg'
 password = 'TestPassword1'
 
-url = 'https://pikabu.ru'
+url = 'https://pikabu.ru/subs'
 sess = requests.Session()
-sess.verify = True
 
-resp = sess.post(url, headers=user_agent, data={'username': username, 'password': password})
-resp.raise_for_status()
+resp = sess.post(url, headers=headers, data={'username': username, 'password': password})
 
-print(sess.cookies)
-resp = sess.get(url + '/subs')
-resp.raise_for_status()
+soup = BeautifulSoup(resp.text, 'html.parser')
+articles_of_stories.extend(soup.find_all('article'))
 
-print(resp.text.count('ilineserg'))
+"""while len(articles_of_stories) < 100:
+    page = 2
+    resp = sess.post(url + f'?page={page}', headers=headers, data={'username': username, 'password': password})
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    articles_of_stories.extend(soup.find_all('article'))
+    page += 1"""
+
+"""if len(articles_of_stories) > 100:
+    while len(articles_of_stories) != 100:
+        articles_of_stories.pop()
+
+print(len(articles_of_stories))"""
+
+
+with open("file.html", "w") as articles:
+    #articles.write(str(articles_of_stories[0]))
+    articles.write(resp.text)
