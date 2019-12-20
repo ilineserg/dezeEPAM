9f67f967ea7a07be8cf96a2590d509bd40fb1299
Sergey ILIN <ilineserg@gmail.com>
2019-12-20 16:19:35 +0300

    done oop_2.py

--- 05-OOP_Exceptions/oop_2.py
+++ 05-OOP_Exceptions/oop_2.py
@@ -43,7 +43,7 @@ from collections import defaultdict
 
 
 class Person:
-    def __init__(self, last_name, first_name):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
