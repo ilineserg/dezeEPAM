06df84d9f025380857ade52de1432c335ef14add
Sergey ILIN <ilineserg@gmail.com>
2020-01-08 22:22:38 +0300

    pikabu done

--- 09-HTTP/pikabu_parser.py
+++ 09-HTTP/pikabu_parser.py
@@ -1,44 +1,85 @@
+import os
+
 from bs4 import BeautifulSoup
+from collections import Counter
 import requests
-from requests.auth import HTTPBasicAuth
-
-articles_of_stories = []
-
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Connection": "keep-alive",
-    "cache-control": "max-age=0",
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
-    "cookie": "pkbRem=%7B%22uid%22%3A3011766%2C%22username%22%3A%22ilineserg%22%2C%22rem%22%3A%226aebdf22bb6e139da1a0dc5b033ae8de%22%2C%22tries%22%3A0%7D"
-}
-
-username = 'ilineserg'
-password = 'TestPassword1'
-
-url = 'https://pikabu.ru/subs'
-sess = requests.Session()
-
-resp = sess.post(url, headers=headers, data={'username': username, 'password': password})
-
-soup = BeautifulSoup(resp.text, 'html.parser')
-articles_of_stories.extend(soup.find_all('article'))
-
-"""while len(articles_of_stories) < 100:
-    page = 2
-    resp = sess.post(url + f'?page={page}', headers=headers, data={'username': username, 'password': password})
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    articles_of_stories.extend(soup.find_all('article'))
-    page += 1"""
-
-"""if len(articles_of_stories) > 100:
-    while len(articles_of_stories) != 100:
-        articles_of_stories.pop()
-
-print(len(articles_of_stories))"""
 
 
-with open("file.html", "w") as articles:
-    #articles.write(str(articles_of_stories[0]))
-    articles.write(resp.text)
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+SUBS_URL = "https://pikabu.ru/new/subs"
+
+COOKIE = "INPUT YOUR COOKIE HERE"
+MAX_ARTICLES = 100
+
+
+def article_parser(text):
+    soup = BeautifulSoup(text, 'lxml')
+    container = soup.find('div',  class_="stories-feed__container")
+    articles = container.find_all('div', class_="story__main")
+    result = []
+    for article in articles:
+        title = article.find('a', class_="story__title-link")
+        if title:
+            title = title.text.strip()
+        else:
+            continue
+
+        tags_container = article.find('div', class_="story__tags")
+        tags = tags_container.find_all('a', class_="tags__tag")
+        tags = list(map(lambda a: a.text.strip(), tags))
+        tags = list(filter(lambda a: a != '[моё]', tags))
+
+        if not all([title, tags]):
+            continue
+        result.append((title, tags))
+
+    return result
+
+
+def main():
+    articles_of_stories = []
+    tags_counter = Counter()
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+        "cookie": COOKIE,
+    }
+
+    session = requests.Session()
+    session.headers.update(headers)
+
+    count = 0
+    page = 1
+
+    while count < MAX_ARTICLES:
+        params = {}
+        if page != 1:
+            params.update({"page": page})
+
+        r = session.get(url=SUBS_URL, params=params, allow_redirects=True)
+
+        print(r.url)
+        print("page", page)
+
+        for title, tags in article_parser(text=r.text):
+            if count >= MAX_ARTICLES:
+                break
+            articles_of_stories.append((title, tags))
+            for tag in tags:
+                tags_counter[tag] += 1
+            count += 1
+
+        print("count", count)
+        page += 1
+
+    with open(os.path.join(BASE_PATH, "most_popular_tag.txt"), "w") as tags_file:
+        most_popular_tags = tags_counter.most_common(10)
+        tags_file.write(f"Most popular tags:\n")
+
+        for idx, tag in enumerate(most_popular_tags):
+            tag_name, tag_mentions = tag
+            tags_file.write(f"{idx + 1}. \"{tag_name}\": {tag_mentions}\n")
+
+
+if __name__ == "__main__":
+    main()
